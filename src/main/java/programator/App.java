e34b4cb8c2a1e3c0dce0b454a92cee62f9dfc170
Krzysztof Dyla <57840067+ghostdance386@users.noreply.github.com>
2019-12-02 20:30:55 +0100

    First Stage completed
    - used File and Scanner as input and reading tools
    - used long variable with System.currentTimeMillis() method to measure time
    -solution accepted by Hyperskill

--- src/main/java/programator/App.java
+++ src/main/java/programator/App.java
@@ -1,13 +1,57 @@
 package programator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Krzysztof!" );
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        File directory = new File("src/directory.txt");
+        File find = new File("src/find.txt");
+
+        Map<String, String> phoneBook = new HashMap<>();
+        List<String> namesToFind = new ArrayList<>();
+
+        long startTime;
+        long endTime;
+
+        startTime = System.currentTimeMillis();
+        try (Scanner scannerDirectory = new Scanner(directory)) {
+            while (scannerDirectory.hasNextLine()) {
+                String[] contact = scannerDirectory.nextLine().split("\\s", 2);
+                if (contact.length >= 2) {
+                    phoneBook.put(contact[1], contact[0]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        try (Scanner scannerFind = new Scanner(find)) {
+            while (scannerFind.hasNextLine()) {
+                namesToFind.add(scannerFind.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Start searching");
+        int foundCount = 0;
+        int searchesCount = namesToFind.size();
+        for (String name : namesToFind) {
+            if (phoneBook.containsKey(name)) {
+                phoneBook.get(name);
+                foundCount++;
+            }
+        }
+        endTime = System.currentTimeMillis();
+        long minute = (endTime - startTime) / 1000 / 60;
+        long seconds = (endTime - startTime) / 1000-(minute*60);
+        long milliseconds = (endTime - startTime)-(seconds*1000)-(minute*60);
+        System.out.println("Found " + foundCount + "/" + searchesCount + " .Time taken: " + minute + "min. " + seconds + "sec. " + milliseconds + "ms.");
+
     }
+
 }
